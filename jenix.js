836bcbb5d8e0f14cf5f14952c2b17b5cd74cf280
Daniel-Oshione <omobsdaniel@gmail.com>
2024-02-17 10:30:46 +0100

    added comment line

--- jenix.js
+++ jenix.js
@@ -1,9 +1,4 @@
-// const drop_down = document.querySelector('.drop_down');
-// const toggle = document.getElementById('toggle');
 
-// toggle.onclick = function(){
-//   drop_down.classList.toggle('active');
-// }
 document.addEventListener('DOMContentLoaded', function() {
   const toggleButton = document.getElementById('toggle');
   const dropDown = document.querySelector('.drop_down');
