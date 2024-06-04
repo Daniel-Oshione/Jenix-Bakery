70112cc69cc9de8252601333aeac13142b19ce31
Daniel-Oshione <omobsdaniel@gmail.com>
2024-06-04 11:47:20 +0100

    fixed some issues with font and updated services page

--- jenix.js
+++ jenix.js
@@ -1,4 +1,4 @@
-
+// for mobile nav bar
 document.addEventListener('DOMContentLoaded', function() {
   const toggleButton = document.getElementById('toggle');
   const dropDown = document.querySelector('.drop_down');
@@ -7,3 +7,11 @@ document.addEventListener('DOMContentLoaded', function() {
     dropDown.classList.toggle('visible');
   });
 });
+// for the services page
+function showContent(category) {
+  const allContent = document.querySelectorAll('div[id^="Bread"], div[id^="Cakes"], div[id^="Snacks"], div[id^="Parfait"]');
+  allContent.forEach(content => {
+      content.style.display = 'none';
+  });
+  document.getElementById(category).style.display = 'block';
+}
