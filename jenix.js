037ac6f48103995abe0b937999ebe92f9057bd35
Daniel-Oshione <omobsdaniel@gmail.com>
2024-09-05 14:02:10 +0100

    fixed some ui problems

--- jenix.js
+++ jenix.js
@@ -7,6 +7,7 @@ document.addEventListener('DOMContentLoaded', function() {
     dropDown.classList.toggle('visible');
   });
 });
+
 // for the services page
 function showContent(category) {
   const allContent = document.querySelectorAll('div[id^="Bread"], div[id^="Cakes"], div[id^="Snacks"], div[id^="Parfait"]');
