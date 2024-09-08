64ba3cacd2c7ba8370bc00d359dcb2177e47221f
Daniel-Oshione <omobsdaniel@gmail.com>
2024-09-08 17:49:34 +0100

    major services page update

--- jenix.js
+++ jenix.js
@@ -11,8 +11,18 @@ document.addEventListener('DOMContentLoaded', function() {
 // for the services page
 function showContent(category) {
   const allContent = document.querySelectorAll('div[id^="Bread"], div[id^="Cakes"], div[id^="Snacks"], div[id^="Parfait"]');
+  const allLinks = document.querySelectorAll('a[href="#"]');
+
   allContent.forEach(content => {
-      content.style.display = 'none';
+    content.style.display = 'none';
   });
-  document.getElementById(category).style.display = 'block';
+  allLinks.forEach(link => {
+    link.classList.remove('show-shadow');
+  });
+
+  // Set display style based on category
+  const gridCategories = ['Bread', 'Snacks', 'Parfait'];
+  document.getElementById(category).style.display = gridCategories.includes(category) ? 'grid' : 'block';
+
+  document.querySelector(`a[onclick="showContent('${category}')"]`).classList.add('show-shadow');
 }
